9232ceb7bf797885b836bb73d2603be7945ccf51
Jeremy Hand <jeremyhand3.14@gmail.com>
2025-03-04 09:09:28 -0600

    2

--- manage.py
+++ manage.py
@@ -15,6 +15,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    if len(sys.argv) == 1:
+        sys.argv.append("runserver")
+        sys.argv.append("8080")
     execute_from_command_line(sys.argv)
 
 
